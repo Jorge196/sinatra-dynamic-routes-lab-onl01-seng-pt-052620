106bfaa4f7ec6ddfa3264ffabf05af4fc8f14f30
Jorge Ramirez <joramirez96@gmail.com>
2020-08-27 00:20:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,6 +5,13 @@ class App < Sinatra::Base
   get '/reversename/:name' do
     @reverse_name = params[:name].reverse
     @reverse_name
-  end 
+  end
+
+  get '/square/:number' do
+    @num = params[:number].to_i ** 2
+    @num.to_s
+  end
+
+  
 
 end
